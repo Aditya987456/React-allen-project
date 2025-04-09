132dc06214f8f3be014f23672dc66686dc14b6e2
Aditya raj <adityaraj987456@gmail.com>
2025-04-09 23:21:12 +0530

    starting the project structuring pages

---
+++ react-allen/src/layout/mainlayout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Navbar = () => {
+    return (
+        <div>
+            <h1>helllllllllllllllllo</h1>
+        </div>
+    );
+};
+
+export const Footer = () => {
+    return (
+        <div>
+            <p>This is the Footer</p>
+        </div>
+    );
+};
